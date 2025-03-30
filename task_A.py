0aaf1e1d7f3f6b27cf5529ef7ab90aea31530f7a
Aaron Cayanan <aaroncayanan02@gmail.com>
2025-03-30 15:41:28 -0700

    fix bug on incrementing count during errors, implement inconsistency finding

--- task_A.py
+++ task_A.py
@@ -1,9 +1,10 @@
 class task_A():
     def __init__(self):
-        self.guess_count = 0
+        self.guess_count = 1
         self.min = 0
         self.max = 100
         self.current_guess = (self.min + self.max) / 2
+        self.previous_guess = 1337
         print("Think of a number between 1 and 100!")
 
     def increment_guess_count(self):
@@ -12,18 +13,24 @@ class task_A():
     def play_game(self):
         user_answer = ""
         while user_answer != '=':
-            self.increment_guess_count()
+            if int(self.current_guess) == int(self.previous_guess):
+                print("Hmm... I think you are lying.. Terminating game.")
+                return 1
             print(f"Is your number greater (>), equal (=), or less (<) than {int(self.current_guess)}?")
             user_answer = input("Please answer (<, =, >): ")
             match user_answer:
                 case '<':
+                    self.increment_guess_count()
                     # must be in between min and current guess
                     # set max to current guess
+                    self.previous_guess = self.current_guess
                     self.max = self.current_guess
                     self.current_guess = (self.min + self.max) / 2
                 case '>':
+                    self.increment_guess_count()
                     # must be in between max and current guess
                     # set min to current guess
+                    self.previous_guess = self.current_guess
                     self.min = self.current_guess
                     self.current_guess = (self.min + self.max) / 2
                 case '=':
